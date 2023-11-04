5b19f4ad6f811b31faf2860608097768741943cd
Jan Michael Biyo <janmichael.biyo@gmail.com>
2023-11-03 23:10:22 -0600

    i answered the terminal txt questions

---
+++ assignment/helloWorld.js
@@ -0,0 +1,26 @@
+// Code Comment
+
+console.log('hi ya');
+
+//Variables 
+// let
+const firstName = 'jan michael'
+
+// conditionals
+
+// if, else if, else
+// executing code is inside currly brackets.
+
+//== same value not same type
+//12=='12' true
+// === same value same type
+// 12==='12' false
+// != does not equal
+// !== 
+// >
+// <
+// >=
+// && 
+// !
+// flasy value: undefined, null, 0
+
